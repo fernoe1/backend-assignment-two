0e3f53af2db0fb34748c3c81810305b80e13b6d7
fernoe1 <krutoytemirlan2007@mail.ru>
2025-12-24 19:56:50 +0600

    feat: exhange USD

--- client/src/components/ExchangeSection.jsx
+++ client/src/components/ExchangeSection.jsx
@@ -4,6 +4,7 @@ const ExchangeSection = ({ exchange }) => {
     return (
         <div className={s.exchangeSection}>
             <p>1 {exchange.baseCode} is {exchange.conversionResult} KZT</p>
+            <p>1 {exchange.baseCode} is {exchange.usdConversionResult} USD</p>
         </div>
     )
 }
--- server/services/currency.service.js
+++ server/services/currency.service.js
@@ -7,12 +7,17 @@ export const getCurrency = async (currencyCode) => {
     if (!res.ok) {
         throw new Error(`API error: ${res.status}`);
     }
+    const resTwo = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_API_KEY}/pair/${currencyCode}/USD/1`);
+    if (!resTwo.ok) {
+        throw new Error(`API error: ${resTwo.status}`);
+    }
 
     const json = await res.json();
-    console.log(json);
+    const jsonTwo = await resTwo.json();
 
     return {
         baseCode: json.base_code,
         conversionResult: json.conversion_result,
+        usdConversionResult: jsonTwo.conversion_result,
     };
 }
